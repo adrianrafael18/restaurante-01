bf6525f44b50818082c99eff7c689dc5652fe234
ADRIAN <114356@mail.unacar.mx>
2022-02-23 09:36:21 -0600

    elimine boton de acsion

--- istrucsiones .js
+++
@@ -1,8 +0,0 @@
-<function capture ()>
-       <alert ("cnatidad") >
-           <var cantidad = document . get ElementBy Id("productos a vender") .value>
-<input type="text" name="cnatidad" id="productos a vender" placeholder="unidades">
-                <input type="submit" onclick="capture" value="comprar"></td>
-           </alert>
-        </var>
-    </function>
